447c73c1527eed8b58bd3d6ff00a2b3b0a0d8080
braelynmuskwe <braemira1@icloud.com>
2018-06-20 17:40:43 +0000

    automatically backed up by learn

---
+++ hotel_welcome.rb
@@ -0,0 +1,6 @@
+def tv_welcome(name, room_number)
+  puts "Welcome #{name}"
+  puts "to the Springhill Suites"
+  puts "room #{room_number}"
+end
+tv_welcome("Braelyn", "212")
---
+++ loops.rb
@@ -0,0 +1,15 @@
+def kwk
+  days = 2
+  days_left = 7
+  while days < 10 
+  puts "keep coming to camp!!!"
+  puts "you have #{days_left}"
+  #days += 1
+  days_left -= 1
+  end
+  puts "camp is over"
+end
+
+kwk
+
+
